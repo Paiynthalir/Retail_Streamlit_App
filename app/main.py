f6abc6fa260bbce3d2a0ad6efcfb51d4d4c72b05
Pine Nallamuthu <paiynthalir.samyappannallamuthu@student.uts.edu.au>
2024-10-05 08:37:49 +1000

    first commit

---
+++ app/main.py
@@ -0,0 +1,6 @@
+# Streamlit APP to consume the APIs hosted at https://retail-fastapi-app.onrender.com 
+# This app is used for
+ # 1. Predict the sales revenue for a given item in a specific store at a given date.
+ # 2. Forecast the total sales revenue across all stores and items for the next 7 days
+
+
